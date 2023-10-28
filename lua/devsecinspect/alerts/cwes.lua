17e28ae20cdc71ee3aa6d4e66857f3cfbffe202b
GeekMasher <geekmasher@gmail.com>
2023-10-28 12:55:55 +0100

    feat(cwes): Add CWEs library

---
+++ lua/devsecinspect/alerts/cwes.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.cwes = {
+    ["CWE-20"] = "Improper Input Validation",
+    ["CWE-22"] = "Path Traversal",
+    ["CWE-78"] = "OS Command Injection",
+    ["CWE-79"] = "Cross-site Scripting (XSS)",
+    ["CWE-89"] = "SQL Injection",
+    ["CWE-200"] = "Information Exposure",
+    ["CWE-287"] = "Improper Authentication",
+    ["CWE-327"] = "Use of a Broken or Risky Cryptographic Algorithm",
+    ["CWE-352"] = "Cross-Site Request Forgery (CSRF)",
+    ["CWE-434"] = "Unrestricted File Upload",
+    ["CWE-502"] = "Deserialization of Untrusted Data",
+    ["CWE-601"] = "Open Redirect",
+    ["CWE-798"] = "Use of Hard-coded Credentials",
+    ["CWE-918"] = "Server-Side Request Forgery (SSRF)",
+}
+
+--- Get CWE name from CWE ID
+---@param cwe any
+---@return string
+function M.get_cwe(cwe)
+    return M.cwes[cwe]
+end
+
+return M
