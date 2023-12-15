cbca3c4e02dcee94a3fea0381039ae2db587c535
GeekMasher <geekmasher@gmail.com>
2023-12-15 13:22:47 +0000

    style: Update tests

--- tests/init.lua
+++ tests/init.lua
@@ -11,30 +11,29 @@ end
 
 ---@param plugin string
 function M.load(plugin)
-    local name = plugin:match(".*/(.*)")
-    local package_root = M.root(".tests/site/pack/deps/start/")
+    local name = plugin:match ".*/(.*)"
+    local package_root = M.root ".tests/site/pack/deps/start/"
     if not vim.loop.fs_stat(package_root .. name) then
         print("Installing " .. plugin)
         vim.fn.mkdir(package_root, "p")
-        vim.fn.system({
+        vim.fn.system {
             "git",
             "clone",
             "--depth=1",
             "https://github.com/" .. plugin .. ".git",
             package_root .. "/" .. name,
-        })
+        }
     end
 end
 
 function M.setup()
-    vim.cmd([[set runtimepath=$VIMRUNTIME]])
+    vim.cmd [[set runtimepath=$VIMRUNTIME]]
     vim.opt.runtimepath:append(M.root())
-    vim.opt.packpath = { M.root(".tests/site") }
+    vim.opt.packpath = { M.root ".tests/site" }
 
-    M.load("MunifTanjim/nui.nvim")
+    M.load "MunifTanjim/nui.nvim"
 
     require("devsecinspect").setup()
 end
 
 M.setup()
-
